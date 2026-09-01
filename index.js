ff3d8f33a70f3face749b0e9f4df35db1a05fd73
Oluwasegun <oakinwande671@gmail.com>
2026-09-01 16:19:12 +0100

    segun is cool

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "segun-store.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
